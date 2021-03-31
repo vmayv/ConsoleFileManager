7dd927ac0ea430f6ecc8fb8b97ff19d0cb66d6a2
vmayv <vasyamayorov@yandex.ru>
2021-03-31 12:28:54 +0300

    Добавлены комментарии к config.cs

--- ConsoleFileManager/Config.cs
+++ ConsoleFileManager/Config.cs
@@ -8,26 +8,61 @@ namespace ConsoleFileManager
 {
     public partial class Program
     {
+        /// <summary>
+        /// Получить текущую директорию из свойств
+        /// </summary>
+        /// <returns></returns>
         static string GetCurrentDirectory() => Properties.Settings.Default.CurrentDirectory;
+        /// <summary>
+        /// Установить текущую директорию в свойства
+        /// </summary>
+        /// <param name="directory"></param>
         static void SetCurrentDirectory(string directory)
         {
             Properties.Settings.Default.CurrentDirectory = directory;
             Properties.Settings.Default.Save();
         }
+        /// <summary>
+        /// Получить высоту экрана
+        /// </summary>
+        /// <returns></returns>
         static int GetScreenHeight() => Properties.Settings.Default.ScreenHeight;
+        /// <summary>
+        /// Получить ширину экрана
+        /// </summary>
+        /// <returns></returns>
         static int GetScreenWidth() => Properties.Settings.Default.ScreenWidth;
+        /// <summary>
+        /// Получить уровень вложенности каталогов
+        /// </summary>
+        /// <returns></returns>
         static int GetEnclosureLevel() => Properties.Settings.Default.EnclosureLevel;
+        /// <summary>
+        /// Задать уровень вложенности каталогов
+        /// </summary>
+        /// <param name="count"></param>
         static void SetEnclosureLevel(int count)
         {
             Properties.Settings.Default.EnclosureLevel = count;
             Properties.Settings.Default.Save();
         }
+        /// <summary>
+        /// Получить количество элементов на странице
+        /// </summary>
+        /// <returns></returns>
         static int GetCountElementsOnPage() => Properties.Settings.Default.CountElementsOnPage;
+        /// <summary>
+        /// Задать количество элементов на странице
+        /// </summary>
+        /// <param name="count"></param>
         static void SetCountElementsOnPage(int count)
         {
             Properties.Settings.Default.CountElementsOnPage = count;
             Properties.Settings.Default.Save();
         }
+        /// <summary>
+        /// Первоначальная инициализация конфига
+        /// </summary>
         static void InitializeConfig()
         {
             if (Properties.Settings.Default.ScreenHeight == 0 || Properties.Settings.Default.ScreenWidth == 0)
